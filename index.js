fe073bfe5c4b3b8d95778c3d5d32a8be51942477
Xuan-An Cao <caoxantb@gmail.com>
2021-11-02 02:32:58 +0200

    uibuild

---
+++ index.js
@@ -0,0 +1,105 @@
+require("dotenv").config();
+
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+
+const Person = require("./models/person");
+
+const app = express();
+
+morgan.token("person", (req) => JSON.stringify(req.body));
+
+app.use(express.static("build"));
+app.use(express.json());
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :person"
+  )
+);
+app.use(cors());
+
+app.get("/api/persons", (req, res) => {
+  Person.find({}).then((persons) => {
+    res.json(persons);
+  });
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  Person.findById(req.params.id)
+    .then((person) => {
+      if (person) res.json(person);
+      else res.status(404).end();
+    })
+    .catch((error) => next(error));
+});
+
+app.delete("/api/persons/:id", (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then((result) => {
+      res.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+
+app.post("/api/persons", (req, res, next) => {
+  const body = req.body;
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  });
+
+  person
+    .save()
+    .then((savedPerson) => savedPerson.toJSON())
+    .then((savedAndFormattedPerson) => {
+      res.json(savedAndFormattedPerson);
+    })
+    .catch((error) => next(error));
+});
+
+app.put("/api/persons/:id", (req, res, next) => {
+  const body = req.body;
+
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: "info missing",
+    });
+  }
+
+  if (isNaN(body.number.replace(/-/g, "")) || body.number === "") {
+    return res.status(400).json({
+      error: `${body.number} is not a valid telephone number`,
+    });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      res.json(updatedPerson);
+    })
+    .catch((error) => next(error));
+});
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return res.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return res.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT);
+console.log(`Server running on port ${PORT}`);
---
+++ models/person.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const uniqueValidator = require("mongoose-unique-validator");
+
+const URL = process.env.MONGODB_URI;
+
+mongoose
+  .connect(URL)
+  .then((result) => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message);
+  });
+
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    minLength: 3,
+  },
+  number: {
+    type: String,
+    required: true,
+    minLength: 8,
+    validate: {
+      validator: (num) => {
+        return !isNaN(num.replace(/-/g, ""));
+      },
+      message: "Phone number invalid",
+    },
+  },
+});
+
+personSchema.plugin(uniqueValidator);
+
+personSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model("Person", personSchema);
